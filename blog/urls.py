f0ff6eb240e738b35cd3a6a44eb66d25c69a8b0a
Hov007 <hovayvazyan7@gmail.com>
2022-04-09 15:52:55 +0400

    DetailView, tests

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', BlogListView.as_view(), name='home')
+    path('', BlogListView.as_view(), name='home'),
+    path('post/<int:pk>', BlogDetailView.as_view(), name='post_detail'),  # new
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView  # new
 
 from .models import *
 
@@ -8,3 +8,7 @@ class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
     context_object_name = 'all_posts_list'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
